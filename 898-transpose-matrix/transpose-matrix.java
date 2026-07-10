14415c8065419772d5ec39599aa370a000b97c56
soni-anwita-12 <anwitasoni813@gmail.com>
2026-07-10 11:06:11 +0530

    Time: 1 ms (25.72%) | Memory: 46.4 MB (95.94%) - LeetSync

---
+++ 898-transpose-matrix/transpose-matrix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] transpose(int[][] a) {
+        int row=a.length;
+        int col=a[0].length;
+        int[][]b=new int[col][row];
+        for(int i=0;i<b.length;i++){
+            for(int j=0;j<b[0].length;j++){
+                b[i][j]=a[j][i];
+            }
+
+        }
+        return b;
+        
+    }
+}
