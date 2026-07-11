0e915a1afeb0bf4ce0c0b41ad14abf81707d5a53
soni-anwita-12 <anwitasoni813@gmail.com>
2026-07-11 23:41:10 +0530

    Time: 24 ms (25.13%) | Memory: 81.5 MB (79.43%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] arr) {
+        Arrays.sort(arr);
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]==arr[i-1]){
+                return true;
+            }
+        }
+        return false;
+
+        
+    }
+}
