8a6d29f214aee35a5b59082cc07a51018ee779e6
soni-anwita-12 <anwitasoni813@gmail.com>
2026-07-20 20:37:29 +0530

    Time: 306 ms (5.07%) | Memory: 47.8 MB (38.46%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,16 +1,15 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        int j=0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                j++;
+    public void moveZeroes(int[] arr) {
+         int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]==0){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
             }
         }
-        
-       
         
     }
 }
