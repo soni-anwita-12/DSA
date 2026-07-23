d6f0cfdffd6bfa42a0010dc3c3b0f5b0e07a655d
soni-anwita-12 <anwitasoni813@gmail.com>
2026-07-23 18:39:40 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (85.22%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int n=arr.length;
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]>target){
+                high=mid-1;
+            }else if(arr[mid]==target){
+                return mid;
+            }else{
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
+}
