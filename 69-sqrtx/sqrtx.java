5f6e6369fa760f7a31521e06f06cb581c3531024
soni-anwita-12 <anwitasoni813@gmail.com>
2026-07-24 22:52:42 +0530

    Time: 1 ms (97.44%) | Memory: 42.9 MB (8.30%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,25 +1,13 @@
 class Solution {
     public int mySqrt(int n) {
-        if (n < 2) {
-            return n;
-        }
-
-        long lo = 1, hi = n;
-        long ans = 0;
-
-        while (lo <= hi) {
-            long mid = lo + (hi - lo) / 2;
-
-            if (mid * mid == n) {
-                return (int) mid;
-            } else if (mid * mid < n) {
-                ans = mid;       // possible answer
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
-            }
-        }
-
-        return (int) ans;
+        if(n==0) return 0;
+        int lo=1,hi=n; 
+        while(lo<=hi){ 
+            int mid=lo+(hi-lo)/2; 
+            if(mid==n/mid) return mid; 
+            else if(mid>n/mid) hi=mid-1; 
+            else lo=mid+1;
+         }
+          return hi;
     }
 }
