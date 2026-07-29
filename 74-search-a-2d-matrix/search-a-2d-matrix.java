85ab419b5ba64e685874554987dd8ffd41728eab
soni-anwita-12 <anwitasoni813@gmail.com>
2026-07-29 17:34:31 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (43.97%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean searchMatrix(int[][] arr, int tar) {
+        int rows=arr.length , cols=arr[0].length;
+        int lo=0,hi=rows*cols-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            int midRow=mid/cols , midCol=mid%cols;
+            if(arr[midRow][midCol]==tar) return true;
+            else if(arr[midRow][midCol]>tar) hi=mid-1;
+            else lo=mid+1;
+        }
+        return false;
+    }
+}
